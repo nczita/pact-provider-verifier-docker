33ccef6b916e02e11a5256fb18e8f70fa67e4c3d
Łukasz Dziewanowski <lukasz.dziewanowski@airhelp.com>
2019-04-03 14:03:14 +0200

    Clean up pacts after, rubocop

--- src/json_helper.rb
+++ src/json_helper.rb
@@ -3,26 +3,26 @@ require 'faraday_middleware'
 require 'json'
 
 def get_json(path)
-    case path
-    when URI::regexp
-        return get_json_from_server(path)
-    else
-        return get_json_from_local_file(path)
-    end
+  case path
+  when URI::DEFAULT_PARSER.make_regexp
+    get_json_from_server(path)
+  else
+    get_json_from_local_file(path)
+  end
 end
 
 def get_json_from_server(path)
-    url = URI.parse(path)
-    conn = Faraday.new("http://#{url.host}:#{url.port}") do |c|
-        c.use FaradayMiddleware::ParseJson
-        c.use Faraday::Adapter::NetHttp 
-    end
+  url = URI.parse(path)
+  conn = Faraday.new("http://#{url.host}:#{url.port}") do |c|
+    c.use FaradayMiddleware::ParseJson
+    c.use Faraday::Adapter::NetHttp
+  end
 
-    response = conn.get(url.request_uri)
-    return response.body
+  response = conn.get(url.request_uri)
+  response.body
 end
 
 def get_json_from_local_file(path)
-    file = File.read(path)
-    return JSON.parse(file)
+  file = File.read(path)
+  JSON.parse(file)
 end
